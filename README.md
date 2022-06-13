5ec88d4545c6f03db42042afbb770b2f94a68cdf
silvazqx <107441266+silvazqx@users.noreply.github.com>
2022-06-13 19:51:42 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# aula-1-
-aula d programação d cria &lt;3
+# SAtemplate
---
+++ sketch.js
@@ -0,0 +1,11 @@
+var nome,idade,cidade
+
+nome ="enzo"
+idade ="14 anos de pura beleza"
+cidade ="o Rio de Janeiro continua lindo"
+
+
+console.log("Good Night rapazes");
+console.log(nome)
+console.log(idade)
+console.log(cidade)
